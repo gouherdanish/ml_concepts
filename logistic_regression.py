6b95335ff2fc1d2e002cab27ab3fe7a20c848f3f
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-08-12 09:56:55 +0530

    Logistic Regression - fit method added

--- logistic_regression.py
+++ logistic_regression.py
@@ -6,6 +6,7 @@ class LogisticRegression:
         self.iterations = iterations
         self.b0 = 0
         self.b1 = 0
+        self.loss_history = []
 
     def _bdot(self,z):
         return self.b0 + self.b1*z
@@ -14,7 +15,21 @@ class LogisticRegression:
         return 1/(1 + np.exp(-self._bdot(z)))
     
     def predict(self,X):
-        return np.where(self._bdot(X) > 0,1,0)
+        return self._sigmoid()
+        # return np.where(self._bdot(X) > 0,1,0)
 
     def fit(self,X,y):
-        pass
+        n = len(X)
+        y_pred = self.predict(X)
+        residual = y - y_pred
+        loss = (1.0/n)*sum(residual**2)
+        self.loss_history.append(loss)
+
+        db0 = (-2/n) * residual * 1
+        db1 = (-2/n) * residual * X
+
+        self.b0 += (-1 * self.learning_rate * db0)
+        self.b1 += (-1 * self.learning_rate * db1)
+
+    def history(self):
+        return self.loss_history
