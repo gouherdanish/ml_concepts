52b12fbc9a7ab37d18a9e2fc25f45bc801d46e30
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-08-12 09:45:31 +0530

    Logistic Regression - predict function added

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class LogisticRegression:
     def __init__(self,learning_rate=0.01,iterations=100) -> None:
         self.learning_rate = learning_rate
@@ -5,4 +7,14 @@ class LogisticRegression:
         self.b0 = 0
         self.b1 = 0
 
+    def _bdot(self,z):
+        return self.b0 + self.b1*z
+
+    def _sigmoid(self,z):
+        return 1/(1 + np.exp(-self._bdot(z)))
     
+    def predict(self,X):
+        return np.where(self._bdot(X) > 0,1,0)
+
+    def fit(self,X,y):
+        pass
