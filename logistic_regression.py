979d53cae2647415af7beecf26ba2817c89a5d2b
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-08-22 09:30:09 +0530

    Extended Logistic Regression to multiple predictor variables

--- logistic_regression.py
+++ logistic_regression.py
@@ -6,58 +6,121 @@ class LogisticRegression:
         - Binary Classifier
             - 0: negative class
             - 1: positive class
-        - Assumes one predictor variable 
-        - Assumes the classification threshold as 0.5 
-        - Uses cross entropy loss function 
+        - Extended for multiple predictor variable 
     """
     def __init__(self,learning_rate=0.01,iterations=100) -> None:
         self.learning_rate = learning_rate
         self.iterations = iterations
-        self.b0 = 0
-        self.b1 = 0
-        self.loss_history = []
+        self._loss_history = []
 
-    def _linear_model(self,z):
-        return self.b0 + self.b1*z
+    def __init_params__(self,n):
+        """
+        Initializes model parameters
+        - weights and bias of the linear model are the only parameters the model will learn
+        - Iniatization is assumed to be fixed and all zeros
 
-    def _sigmoid(self,z):
-        return 1/(1 + np.exp(-self._linear_model(z)))
+        Args:
+            n - represents number of feature variables
+
+            _weights - vector of shape (n*1) all initialized to 0
+            _bias - scalar initialized to 0
+        """
+        self._weights = np.zeros(n)
+        self._bias = 0
+
+    def parameters(self):
+        return self._weights, self._bias
+    
+    def _linear_model(self,X):
+        """
+        Applies linear model to feature vector X
+
+        Args:
+            X - vector of shape (m*n)
+        
+        Returns:
+            z - vector of shape (m*1)
+        """
+        return self._bias + np.dot(X,self._weights)
+
+    def _sigmoid(self,X):
+        """
+        Calculates sigmoid of X
+
+        Args:
+            X - vector of shape (m*n)
+        
+        Returns:
+            y_prob - represents predicted probabilities - vector of shape (m*1)
+        """
+        return 1/(1 + np.exp(-self._linear_model(X)))
     
     def predict_proba(self,X):
+        """
+        Applies sigmoid function to X
+
+        Args:
+            X: vector having shape (m*n) meaning each example having n predictor variables
+
+        Returns:
+            y: vector having shape (m*1) meaning each example having one predicted probability
+        """
         return self._sigmoid(X)
     
     def predict(self,X):
+        """
+        Calculates Prediction for given X and applies thresholding wrt 0.5 to provide a binary outcome
+        """
         y_prob = self.predict_proba(X)
         return np.where(y_prob > 0.5,1,0)
 
     def fit(self,X,y):
-        n = len(X)
+        """
+        Training function which fits the features X wrt labels y
+        - predicts for given X
+        - finds error in prediction
+        - updates model params (w and b) using gradient descent
+        """
+        # Number of Examples (m) and Features (n)
+        m, n = X.shape
+
+        # Initialize params
+        self.__init_params__(n)
+
+        # Repeat until converged or run for large iterations
         for i in range(self.iterations):
+            # predict
             y_pred = self.predict_proba(X)
+
+            # find error
             residual = y - y_pred
 
+            # track loss
             loss = sum(-y*np.log(y_pred) - (1-y)*np.log(1-y_pred))
-            self.loss_history.append(loss)
+            self._loss_history.append(loss)
 
-            db0 = (-2/n) * sum(residual * 1)
-            db1 = (-2/n) * sum(residual * X)
+            # compute gradient
+            db = (-2/m) * sum(residual)
+            dw = (-2/m) * np.dot(residual,X)
 
-            self.b0 += (-1 * self.learning_rate * db0)
-            self.b1 += (-1 * self.learning_rate * db1)
+            # update model params
+            self._bias += (-1 * self.learning_rate * db)
+            self._weights += (-1 * self.learning_rate * dw)
 
     def history(self):
-        return self.loss_history
+        return self._loss_history
+    
     
 if __name__=='__main__':
-    X = np.array([1,2,-1,5,-2])
+    X = np.array([(-1,1),(2,1),(-1,-1),(5,2),(2,-3)])
     y = np.array([0,1,0,1,0])
 
-    X_test = np.array([3,-3,0.1,-0.1,-0.05,25,-20])
+    X_test = np.array([(3,1),(-3,-1),(-1,3),(-2,-2)])
 
     model = LogisticRegression()
     model.fit(X,y)
     print(model.history())
-    print(model.b0, model.b1)
+    print(model.parameters())
 
     y_val_prob = model.predict_proba(X)
     y_val = model.predict(X)
---
+++ logistic_regression_simple.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+class LogisticRegression:
+    """
+    Features
+        - Binary Classifier
+            - 0: negative class
+            - 1: positive class
+        - Assumes one predictor variable 
+        - Assumes the classification threshold as 0.5 
+        - Uses cross entropy loss function 
+    """
+    def __init__(self,learning_rate=0.01,iterations=100) -> None:
+        self.learning_rate = learning_rate
+        self.iterations = iterations
+        self.b0 = 0
+        self.b1 = 0
+        self.loss_history = []
+
+    def _linear_model(self,z):
+        return self.b0 + self.b1*z
+
+    def _sigmoid(self,z):
+        return 1/(1 + np.exp(-self._linear_model(z)))
+    
+    def predict_proba(self,X):
+        return self._sigmoid(X)
+    
+    def predict(self,X):
+        y_prob = self.predict_proba(X)
+        return np.where(y_prob > 0.5,1,0)
+
+    def fit(self,X,y):
+        n = len(X)
+        for i in range(self.iterations):
+            y_pred = self.predict_proba(X)
+            residual = y - y_pred
+
+            loss = sum(-y*np.log(y_pred) - (1-y)*np.log(1-y_pred))
+            self.loss_history.append(loss)
+
+            db0 = (-2/n) * sum(residual * 1)
+            db1 = (-2/n) * sum(residual * X)
+
+            self.b0 += (-1 * self.learning_rate * db0)
+            self.b1 += (-1 * self.learning_rate * db1)
+
+    def history(self):
+        return self.loss_history
+    
+if __name__=='__main__':
+    X = np.array([1,2,-1,5,-2])
+    y = np.array([0,1,0,1,0])
+
+    X_test = np.array([3,-3,0.1,-0.1,-0.05,25,-20])
+
+    model = LogisticRegression()
+    model.fit(X,y)
+    print(model.history())
+    print(model.b0, model.b1)
+
+    y_val_prob = model.predict_proba(X)
+    y_val = model.predict(X)
+    print(y_val_prob, y_val)
+
+    y_test_prob = model.predict_proba(X_test)
+    y_test = model.predict(X_test)
+    print(y_test_prob, y_test)
