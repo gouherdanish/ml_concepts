a00f60a910619de9150289a20f994a8428d4ed6d
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-08-25 02:52:08 +0530

    Logistic regression example added

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 class LogisticRegression:
     """
@@ -12,6 +13,7 @@ class LogisticRegression:
         self.learning_rate = learning_rate
         self.iterations = iterations
         self._loss_history = []
+        self._gradients = []
 
     def __init_params__(self,n):
         """
@@ -27,9 +29,6 @@ class LogisticRegression:
         """
         self._weights = np.zeros(n)
         self._bias = 0
-
-    def parameters(self):
-        return self._weights, self._bias
     
     def _linear_model(self,X):
         """
@@ -93,40 +92,90 @@ class LogisticRegression:
             y_pred = self.predict_proba(X)
 
             # find error
-            residual = y - y_pred
+            residual = y_pred - y
 
             # track loss
-            loss = sum(-y*np.log(y_pred) - (1-y)*np.log(1-y_pred))
+            loss = (1/m) * sum(-y*np.log(y_pred) - (1-y)*np.log(1-y_pred))
             self._loss_history.append(loss)
 
-            # compute gradient
-            db = (-2/m) * sum(residual)
-            dw = (-2/m) * np.dot(residual,X)
+            # # compute gradient
+            dLdb = (1/m) * sum(residual)
+            dLdw = (1/m) * np.dot(residual,X)
+            self._gradients.append((dLdb,dLdw))
 
             # update model params
-            self._bias += (-1 * self.learning_rate * db)
-            self._weights += (-1 * self.learning_rate * dw)
+            self._bias += (-1 * self.learning_rate * dLdb)
+            self._weights += (-1 * self.learning_rate * dLdw)
+
+            if i == 0 or (i + 1) % 10 == 0:
+                print(f"EPOCH [{i+1}/{self.iterations}]: training loss = {loss}")
 
     def history(self):
         return self._loss_history
+
+    def parameters(self):
+        return self._weights, self._bias
+
+    def gradients(self):
+        return self._gradients
     
     
 if __name__=='__main__':
-    X = np.array([(-1,1),(2,1),(-1,-1),(5,2),(2,-3)])
-    y = np.array([0,1,0,1,0])
+    # Sample Data 1
+    # X = np.array([(-1,1),(2,1),(-1,-1),(5,2),(2,-3)])
+    # y = np.array([0,1,0,1,0])
+    # X_test = np.array([(3,1),(-3,-1),(-1,3),(-2,-2)])
 
-    X_test = np.array([(3,1),(-3,-1),(-1,3),(-2,-2)])
+    """
+    Sample Data 2
 
-    model = LogisticRegression()
+    Let's consider an example for a Binary Classification Problem
+
+    "_Gouher browses movies through OTT and his decision to watch a particular movie is based on its rating and release date._"
+
+    Let's take some historical data from Gouher's OTT watchlist history
+
+    | movie name    | rating | released date | watched | 
+    | ------------- | ------ | ------------- | ------- |
+    | kalki         |   6.2  |     2024      |    1    |
+    | tumbbad       |   7.8  |     2018      |    1    | 
+    | indiana jones |   8.1  |     1990      |    0    | 
+    | Tiger 3       |   4.5  |     2023      |    0    | 
+
+    """
+    # Data Gathering
+    df = pd.DataFrame({
+        'movie':['Kalki','Tumbbad','Indiana Jones','Tiger 3'],
+        'rating':[6.2,7.8,8.1,4.5],
+        'released_date':[2024,2018,1990,2023],
+        'watched':[1,1,0,0]
+    })
+
+    # Feature Engg
+    df['recency'] = pd.Timestamp.today().year - df['released_date']
+
+    # Training Data Prep
+    X = df[['rating','recency']].to_numpy()
+    y = df['watched'].to_numpy()
+
+    # Model Training
+    model = LogisticRegression(iterations=100,learning_rate=0.2)
     model.fit(X,y)
-    print(model.history())
     print(model.parameters())
 
+    # Validating
     y_val_prob = model.predict_proba(X)
     y_val = model.predict(X)
     print(y_val_prob, y_val)
 
+    # Validation Accuracy
+    val_acc = 100*sum(y_val==y)/len(y)
+    print(f'validation accuracy = {val_acc}%')
+
+    # Testing
+    X_test = np.array([8.9,2])  # Checking for The Batman movie
     y_test_prob = model.predict_proba(X_test)
     y_test = model.predict(X_test)
     print(y_test_prob, y_test)
+
 
